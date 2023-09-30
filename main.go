5961b566865ad513d44037a7991fa5a9178d6540
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2023-09-30 16:10:52 -0300

    feat: load env from .env files or from terminal

--- main.go
+++ main.go
@@ -3,20 +3,21 @@ package main
 import (
 	"fmt"
 	"joubini/pkg/aws"
+	"joubini/pkg/utils"
 )
 
 func main() {
-	roleARN := ""
-	sessionName := "test"
-  region := "us-east-1"
-	
+	roleARN := utils.GetEnv("AWS_ROLE_ARN", "")
+	sessionName := utils.GetEnv("AWS_SESSION_NAME", "test")
+  region := utils.GetEnv("AWS_DEFAULT_REGION", "us-east-1")
+  
   cred := aws.STSCredentials(roleARN, sessionName, region)
 
   fmt.Println(*cred.AccessKeyId)
 
-  clientId := ""
-  clientSecret := ""
-  refreshToken := ""
+  clientId := utils.GetEnv("LWA_ID", "")
+  clientSecret := utils.GetEnv("LWA_SECRET", "")
+  refreshToken := utils.GetEnv("LWA_REFRESH_TOKEN", "")
 
   token := aws.GetToken(clientId, clientSecret, refreshToken)
 
--- pkg/utils/env.go
+++ pkg/utils/env.go
@@ -1,10 +1,17 @@
 package utils
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func GetEnv(key, def string) string {
+	if err := godotenv.Load(".env"); err != nil {
+		fmt.Println(".env file not found:", err)
+	}
+
 	val := os.Getenv(key)
 
 	if len(val) > 0 {
